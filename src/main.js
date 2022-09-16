b03f30605e0aa30e47ff7b30b3086ac4548f3f8b
beat_mac <geeun0712@naver.com>
2022-09-16 10:01:22 +0900

    초기 세팅

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import mitt from 'mitt';
+import App from './App.vue';
+import store from './store';
+import router from './router';
 
-createApp(App).mount('#app')
+const emitter = mitt();
+const app = createApp(App);
+app.config.globalProperties.emitter = emitter;
+
+app.use(store);
+app.use(router);
+app.mount('#app');
